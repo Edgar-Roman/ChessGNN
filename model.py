71518e5fb02907f6d0473927009db32121916e74
alinachou <alinchalinch@gmail.com>
2023-12-15 05:13:41 +0800

    Modify model and load from pickle file

--- model.py
+++ model.py
@@ -1,4 +1,6 @@
+import pickle
 import torch
+from torch_geometric.data import Data
 import torch.nn.functional as F
 from torch_geometric.nn import GATConv
 
@@ -20,28 +22,35 @@ class GATChessModel(torch.nn.Module):
 
         return x
 
-data = create_data()
-
-# Hyperparameters
-num_features = 12 * 64
-learning_rate = 0.01
-num_epochs = 100
-
-model = GATChessModel(num_features)
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-loss_func = torch.nn.MSELoss()  # Mean Squared Error for regression
-
-# Training loop
-model.train()
-for epoch in range(num_epochs):
-    optimizer.zero_grad()
-    out = model(data)
-    loss = loss_func(out, data.y)
-    loss.backward()
-    optimizer.step()
-    print(f"Epoch {epoch+1}, Loss: {loss.item()}")
+def load_data_from_pickle(file_path):
+    with open(file_path, 'rb') as file:
+        data = pickle.load(file)
+    x = torch.tensor(data['x'], dtype=torch.float)
+    y = torch.tensor(data['y'], dtype=torch.float)
+    # Assuming you have edge_index information. If not, you will need to create it.
+    edge_index = torch.tensor(...) # replace with actual edge_index
+    return Data(x=x, edge_index=edge_index, y=y)
 
 
-# if __name__ == '__main__':
-#     data = create_data()
-#     model = train(data)
+if __name__ == '__main__':
+    data = load_data_from_pickle('data/subset_games_test.pkl')
+
+    # Hyperparameters
+    num_features = 12 * 64
+    learning_rate = 0.01
+    num_epochs = 100
+
+    model = GATChessModel(num_features)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    loss_func = torch.nn.MSELoss()  # Mean Squared Error for regression
+
+    # Training loop
+    model.train()
+    for epoch in range(num_epochs):
+        optimizer.zero_grad()
+        out = model(data)
+        loss = loss_func(out, data.y)
+        loss.backward()
+        optimizer.step()
+        print(f"Epoch {epoch+1}, Loss: {loss.item()}")
+
